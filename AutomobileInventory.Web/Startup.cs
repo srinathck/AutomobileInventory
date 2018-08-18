43e509343143fd2e9456b9d1be023b4bf1bf1930
srinathck <srinathck@github.com>
2018-08-18 12:19:05 -0500

    test commit

--- AutomobileInventory.Web/Startup.cs
+++ AutomobileInventory.Web/Startup.cs
@@ -13,6 +13,7 @@ namespace AutomobileInventory.Web
             Configuration = configuration;
         }
 
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
